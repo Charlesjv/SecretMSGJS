ab18d438b2981c4438a93657e9b2b1e7c76b43f9
CharlesJV <charles764jaison@gmail.com>
2020-05-25 22:29:30 -0400

    Create your own msg div

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hi There')
+document.querySelector('form').addEventListener('submit',event =>{
+    event.preventDefault();
+
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
+
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+   const linput =  document.querySelector('#link-input');
+    linput.value = `${window.location}#${encrypted}`;
+     linput.select();
+    
+})
